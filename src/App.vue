bebb20a403ef3ae9ef571edb2e227483f5bf8366
ylwdev <ylwdev@devide>
2023-09-19 18:14:54 +0800

    Auto Commit

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,28 @@
+
+
 <template>
-  <div class="ivu-p">
-    <Alert>
-      当前 Vue 版本为 {{ vueVersion }}，View UI Plus 版本为 {{ version }}
-    </Alert>
-    <Input v-model="msg" />
-    <Button class="ivu-mt" type="primary" @click="handleClick">Click Me!</Button>
+  <div>
+    <i-login v-if="!isLogin" name="inscodeName" icon="https://a.png" @loginSuccess="handleLoginSuccess" />
+    <div v-if="isLogin" >登陆成功</div>
   </div>
 </template>
-<script setup>
-import { ref, version as vueVersion } from 'vue' // 从 Vue 中导入 ref 和 vueVersion
-import { version, Message } from 'view-ui-plus' // 从 view-ui-plus 中导入 version 和 Message
-const msg = ref('Hello View Design!') // 创建一个响应式的变量 msg，并将其初始值设置为 'Hello View Design!'
 
-function handleClick() { // 定义一个名为 handleClick 的函数
-  Message.info('Welcome to InsCode') // 在页面上显示一个提示消息
+<script>
+import iLogin from 'view-ui-plus'
+
+export default {
+  components: {
+    iLogin
+  },
+  data() {
+    return {
+      isLogin: false
+    }
+  },
+  methods: {
+    handleLoginSuccess() {
+      this.isLogin = true
+    }
+  }
 }
 </script>
-
-<style></style>
