ba45694676adb684ccbce77d6cebbeb56e07e65e
ylwdev <ylwdev@devide>
2023-09-20 16:36:11 +0800

    Auto Commit

---
+++ src/components/login.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    用户登录组件
+    <Input v-model="userName" />
+    <Input v-model="password" />
+    <Button class="ivu-mt" type="primary" @click="handleLogin">登录</Button>
+  </div>
+</template>
+
+<script>
+import { ref, computed } from 'vue'
+import { Message } from 'view-ui-plus'
+
+export default {
+  name: 'ins-login',
+  emits: ['loginSuccess'],
+  data () {
+    return {
+      userName: '',
+      password: ''
+    }
+  },
+  methods: {
+    handleLogin() {
+      const currentUser = JSON.parse(localStorage.getItem('currentUser'))
+      if (!currentUser || currentUser.userName !== this.userName || currentUser.password !== this.password) {
+        Message.error('用户名或密码错误')
+        return
+      }
+      this.$emit('loginSuccess','list')
+    }
+  }
+}
+</script>
---
+++ src/components/register.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    用户注册组件
+    <Input v-model="form.userName" />
+    <Input v-model="form.password" />
+    <Button class="ivu-mt" type="primary" @click="register">注册</Button>
+  </div>
+</template>
+
+<script>
+import { ref, computed } from 'vue'
+import { Message } from 'view-ui-plus'
+
+export default {
+  name: 'ins-register',
+  emits: ['registerSuccess'],
+  data () {
+    return {
+      form: {
+        userName: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    register () {
+      const { userName, password } = this.form
+      if (!userName || !password) {
+        Message.error('用户名和密码不能为空');
+        return;
+      }
+      localStorage.setItem('currentUser', JSON.stringify({
+        userName,
+        password
+      }));
+      this.$emit('registerSuccess', 'login');
+    },
+  }
+}
+</script>
